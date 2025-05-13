15c48d8249453d46c45d1a0b51f7e014d561a32b
Harizmudrik <muhammadharizmudrik@gmail.com>
2025-05-13 18:53:00 +0700

    Membuat class mahasiswa dan construktor dengan parameter

--- construktorberparameter.cpp
+++ construktorberparameter.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+class mahasiswa {
+    public:
+    //construktor dengan parameter
+    mahasiswa(int nim, string nama);
+};
+
+
 
 int main ()
 {
-    
+
 }
