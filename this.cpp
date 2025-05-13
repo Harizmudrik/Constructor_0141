76b66157ee78c737fc6a615d04ed051adbdad6e4
Harizmudrik <muhammadharizmudrik@gmail.com>
2025-05-13 19:22:11 +0700

    Membuat definisi kelas buku dengan atribut privat judul dan sebuah metode publik setget() yang menerima parameter jdl

--- this.cpp
+++ this.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
 
+class buku {
+    string judul;
+    public:
+        string setget(string jdl)
+        {
+            //nilai parameter 'judul' untuk member variabel 'judul'
+            this->judul = jdl;
+            //return variabel judul
+            return this->judul;
+        }
+}
+
 int main ()
 {
-    
+
 }
